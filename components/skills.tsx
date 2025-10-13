13aa1c83a33788dd48fbc8596b7f1d0f924def26
azzandwi1 <azzandwiriski1@gmail.com>
2025-10-13 12:57:53 +0700

    update interface

--- components/skills.tsx
+++ components/skills.tsx
@@ -78,7 +78,7 @@ export default function Skills() {
 
             <TabsContent value="technical">
               <motion.div
-                className="grid grid-cols-2 md:grid-cols-5 gap-6"
+                className="grid grid-cols-2 md:grid-cols-4 gap-6"
                 variants={staggerContainer}
                 initial="hidden"
                 animate={activeTab === "technical" ? "visible" : "hidden"}
@@ -147,7 +147,7 @@ export default function Skills() {
             Tools & Technologies
           </h3>
           <motion.div
-            className="grid grid-cols-2 md:grid-cols-5 gap-6"
+            className="grid grid-cols-2 md:grid-cols-4 gap-6"
             variants={staggerContainer}
           >
             {[
